d154ffc7c8f57145e39efa01936dfd05a0cace1d
C0mpy <n.zunic@yahoo.com>
2018-03-15 00:01:38 +0100

    add UnitOfMesure model

--- src/main/java/compi/springframework/spring5recipeapp/model/Ingredient.java
+++ src/main/java/compi/springframework/spring5recipeapp/model/Ingredient.java
@@ -16,7 +16,8 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -50,4 +51,11 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
 }
