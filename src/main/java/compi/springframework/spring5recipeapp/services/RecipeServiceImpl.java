2496220edbbc05b32e9528586bbb8517b39817eb
C0mpy <n.zunic@yahoo.com>
2018-03-15 02:15:12 +0100

    add view for Recipes display

--- src/main/java/compi/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/compi/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -3,10 +3,12 @@ package compi.springframework.spring5recipeapp.services;
 import compi.springframework.spring5recipeapp.model.Recipe;
 import compi.springframework.spring5recipeapp.repositories.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
