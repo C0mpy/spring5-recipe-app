2de5bec8c2576d2b6806b1a22fbd3ab09a9b0608
C0mpy <n.zunic@yahoo.com>
2018-03-15 13:37:01 +0100

    refactor code using project Lombok, add minimal logging example to RecipeService

--- src/main/java/compi/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/compi/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package compi.springframework.spring5recipeapp.services;
 
 import compi.springframework.spring5recipeapp.model.Recipe;
 import compi.springframework.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -21,6 +23,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().forEach(recipe -> recipeSet.add(recipe));
         return recipeSet;
