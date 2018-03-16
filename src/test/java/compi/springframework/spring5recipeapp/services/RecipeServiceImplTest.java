72120b46ec78a943c4cd86456450523bf4798335
C0mpy <n.zunic@yahoo.com>
2018-03-16 23:21:42 +0100

    test RecipeService and Category

---
+++ src/test/java/compi/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package compi.springframework.spring5recipeapp.services;
+
+import compi.springframework.spring5recipeapp.model.Recipe;
+import compi.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+
+    }
+
+    @Test
+    public void getRecipes() {
+
+        HashSet<Recipe> recipesData =  new HashSet<>();
+        Recipe recipe = new Recipe();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1, recipes.size());
+    }
+
+}
